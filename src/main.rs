ef772c9643fc36569e5bb7f156de7ae7aeba4bc4
mygnu <tech@gill.net.in>
2022-07-17 22:15:36 +0100

    refactor usb serial

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ mod app {
     use cortex_m::asm::delay;
     use defmt::{debug, trace};
     use opilio_firmware::{
-        controller::Controller, serial_handler, tacho::TachoReader, FlashOps,
-        PwmTimer2,
+        controller::Controller, serial_handler::UsbHandler, tacho::TachoReader,
+        FlashOps, PwmTimer2,
     };
     use opilio_lib::{ConfId, Config, PID, VID};
     use stm32f1xx_hal::{
@@ -29,8 +29,7 @@ mod app {
 
     #[shared]
     struct Shared {
-        usb_dev: UsbDevice<'static, UsbBusType>,
-        serial: usbd_serial::SerialPort<'static, UsbBusType>,
+        usb_handler: UsbHandler<UsbBusType>,
         config: Config,
         flash: Parts,
         controller: Controller,
@@ -100,6 +99,8 @@ mod app {
         .device_class(usbd_serial::USB_CLASS_CDC)
         .build();
 
+        let usb_handler = UsbHandler::new(usb_dev, serial);
+
         // Initialize the monotonic (SysTick rate is 48 MHz)
         let mono = Systick::new(cx.core.SYST, 48_000_000);
 
@@ -148,8 +149,7 @@ mod app {
 
         (
             Shared {
-                usb_dev,
-                serial,
+                usb_handler,
                 config,
                 flash,
                 controller,
@@ -183,7 +183,7 @@ mod app {
         periodic::spawn_after(period.millis()).ok();
     }
 
-    #[task(binds = USB_HP_CAN_TX, shared = [usb_dev, serial, config, flash])]
+    #[task(binds = USB_HP_CAN_TX, shared = [])]
     fn usb_tx(_cx: usb_tx::Context) {
         debug!("usb tx");
     }
@@ -229,11 +229,10 @@ mod app {
 
     /// usb_rx0 interrupt handler
     /// triggers every time there is incoming data on usb serial bus
-    #[task(binds = USB_LP_CAN_RX0, shared = [usb_dev, serial, config, flash, tick, controller, tacho])]
+    #[task(binds = USB_LP_CAN_RX0, shared = [usb_handler, config, flash, tick, controller, tacho])]
     fn usb_rx0(cx: usb_rx0::Context) {
         // debug!("usb rx");
-        let mut usb_dev = cx.shared.usb_dev;
-        let mut serial = cx.shared.serial;
+        let mut usb_handler = cx.shared.usb_handler;
         let mut config = cx.shared.config;
         let mut flash = cx.shared.flash;
         let mut tick = cx.shared.tick;
@@ -246,20 +245,15 @@ mod app {
         });
 
         (
-            &mut usb_dev,
-            &mut serial,
+            &mut usb_handler,
             &mut config,
             &mut flash,
             &mut controller,
             &mut tacho,
         )
-            .lock(
-                |usb_dev, serial, config, flash, controller, tacho| {
-                    serial_handler::usb_poll(
-                        usb_dev, serial, config, flash, controller, tacho,
-                    );
-                },
-            );
+            .lock(|usb_handler, config, flash, controller, tacho| {
+                usb_handler.poll(config, flash, controller, tacho);
+            });
     }
 }
 
--- src/serial_handler.rs
+++ src/serial_handler.rs
@@ -4,106 +4,144 @@ use opilio_lib::{
 };
 use postcard::to_vec;
 use stm32f1xx_hal::flash;
-use usb_device::{bus::UsbBus, prelude::UsbDevice};
+use usb_device::{bus::UsbBus, prelude::UsbDevice, UsbError};
 use usbd_serial::SerialPort;
 
 use crate::{controller::Controller, tacho::TachoReader, Config, FlashOps};
 
-#[inline(always)]
-pub fn usb_poll<B: UsbBus>(
-    usb_dev: &mut UsbDevice<'static, B>,
-    serial: &mut SerialPort<'static, B>,
-    config: &mut Config,
-    flash: &mut flash::Parts,
-    controller: &mut Controller,
-    tacho: &mut TachoReader,
-) {
-    if !usb_dev.poll(&mut [serial]) {
-        return;
-    }
-    if let Err(e) = process_command(serial, config, tacho, controller, flash) {
-        defmt::trace!("usb_poll error: {}", e);
-    }
+pub struct UsbHandler<B: UsbBus + 'static> {
+    pub usb_dev: UsbDevice<'static, B>,
+    pub serial: SerialPort<'static, B>,
 }
 
-#[inline(always)]
-fn process_command<B: UsbBus>(
-    serial: &mut SerialPort<'static, B>,
-    config: &mut Config,
-    tacho: &mut TachoReader,
-    controller: &mut Controller,
-    flash: &mut flash::Parts,
-) -> Result<()> {
-    let mut buf = [0u8; MAX_SERIAL_DATA_SIZE];
-    let count = serial.read(&mut buf).map_err(|_| Error::SerialRead)?;
-
-    if count == 0 {
-        return Ok(());
+impl<B: UsbBus + 'static> UsbHandler<B> {
+    pub fn new(
+        usb_dev: UsbDevice<'static, B>,
+        serial: SerialPort<'static, B>,
+    ) -> Self {
+        Self { usb_dev, serial }
     }
 
-    defmt::debug!("bytes read: {}", count);
-    defmt::trace!("BUF {:?}", buf);
-    if count == 64 {
-        if let Ok(count) = serial.read(&mut buf[count..]) {
-            defmt::debug!("bytes second read: {}", count);
-            defmt::trace!("BUF {:?}", buf);
+    pub fn poll(
+        &mut self,
+        config: &mut Config,
+        flash: &mut flash::Parts,
+        controller: &mut Controller,
+        tacho: &mut TachoReader,
+    ) {
+        if !self.usb_dev.poll(&mut [&mut self.serial]) {
+            return;
+        }
+        if let Err(e) = self.process_command(tacho, config, controller, flash) {
+            defmt::trace!("usb_poll error: {}", e);
         }
     }
-    let otw_in = OTW::from_bytes(&buf)?;
-    defmt::debug!("Received {:?}", otw_in.cmd());
-    match otw_in.cmd() {
-        Cmd::SetConfig => {
-            if let Data::Config(new_config) = otw_in.data() {
-                *config = new_config;
-                let bytes: Vec<u8, 3> = to_vec(&Response::Ok)?;
-                serial
+
+    fn process_command(
+        &mut self,
+        tacho: &mut TachoReader,
+        config: &mut Config,
+        controller: &mut Controller,
+        flash: &mut flash::Parts,
+    ) -> Result<()> {
+        let mut buf = [0u8; MAX_SERIAL_DATA_SIZE];
+        let mut cursor =
+            self.serial.read(&mut buf).map_err(|_| Error::SerialRead)?;
+
+        if cursor == 0 {
+            return Ok(());
+        }
+        defmt::debug!("bytes read: {}", cursor);
+        defmt::debug!("BUF {=[?]}", buf);
+        loop {
+            match self.serial.read(&mut buf[cursor..]) {
+                Ok(count) => {
+                    defmt::debug!("count: {}", count);
+                    if count == 0 {
+                        break;
+                    }
+                    cursor += count
+                }
+                Err(e) => match e {
+                    UsbError::WouldBlock => break,
+                    _ => return Err(Error::SerialRead),
+                },
+            }
+        }
+
+        defmt::debug!("bytes second read: {}", cursor);
+        defmt::debug!("BUF {=[?]}", buf);
+
+        let otw_in = OTW::from_bytes(&buf)?;
+        defmt::debug!("Received {:?}", otw_in.cmd());
+        match otw_in.cmd() {
+            Cmd::SetConfig => {
+                if let Data::Config(new_config) = otw_in.data() {
+                    *config = new_config;
+                    let bytes: Vec<u8, 3> = to_vec(&Response::Ok)?;
+                    self.serial
+                        .write(bytes.as_ref())
+                        .map_err(|_| Error::SerialWrite)?;
+                }
+            }
+            Cmd::GetConfig => {
+                let bytes =
+                    OTW::new(Cmd::Config, Data::Config(config.clone()))?
+                        .to_vec()?;
+                self.serial
                     .write(bytes.as_ref())
                     .map_err(|_| Error::SerialWrite)?;
             }
-        }
-        Cmd::GetConfig => {
-            let bytes = OTW::new(Cmd::Config, Data::Config(config.clone()))?
-                .to_vec()?;
-            serial
-                .write(bytes.as_ref())
-                .map_err(|_| Error::SerialWrite)?;
-        }
-        Cmd::GetStats => {
-            let (pump1_rpm, fan1_rpm, fan2_rpm, fan3_rpm) = tacho.rpm_data();
-            let stats = Stats {
-                pump1_rpm,
-                fan1_rpm,
-                fan2_rpm,
-                fan3_rpm,
-                liquid_temp: controller.get_liquid_temp(),
-                ambient_temp: controller.get_ambient_temp(),
-            };
-            let otw = OTW::new(Cmd::Stats, Data::Stats(stats))?.to_vec()?;
-            serial.write(otw.as_ref()).map_err(|_| Error::SerialWrite)?;
-        }
-        Cmd::SaveConfig => {
-            if let Err(e) = config.save_to_flash(flash) {
-                let otw =
-                    OTW::new(Cmd::Result, Data::Result(Response::Error(e)))?
-                        .to_vec()?;
-                serial.write(otw.as_ref()).map_err(|_| Error::SerialWrite)?;
-                return Err(e);
-            } else {
-                let otw = OTW::new(Cmd::Result, Data::Result(Response::Ok))?
-                    .to_vec()?;
-                serial.write(otw.as_ref()).map_err(|_| Error::SerialWrite)?;
+            Cmd::GetStats => {
+                let (pump1_rpm, fan1_rpm, fan2_rpm, fan3_rpm) =
+                    tacho.rpm_data();
+                let stats = Stats {
+                    pump1_rpm,
+                    fan1_rpm,
+                    fan2_rpm,
+                    fan3_rpm,
+                    liquid_temp: controller.get_liquid_temp(),
+                    ambient_temp: controller.get_ambient_temp(),
+                };
+                let otw = OTW::new(Cmd::Stats, Data::Stats(stats))?.to_vec()?;
+                self.serial
+                    .write(otw.as_ref())
+                    .map_err(|_| Error::SerialWrite)?;
             }
-        }
-        Cmd::SetStandby => {
-            if let Data::U64(sleep_after) = otw_in.data() {
-                config.sleep_after_ms = sleep_after;
-                let otw = OTW::new(Cmd::Result, Data::Result(Response::Ok))?
+            Cmd::SaveConfig => {
+                if let Err(e) = config.save_to_flash(flash) {
+                    let otw = OTW::new(
+                        Cmd::Result,
+                        Data::Result(Response::Error(e)),
+                    )?
                     .to_vec()?;
-                serial.write(otw.as_ref()).map_err(|_| Error::SerialWrite)?;
+                    self.serial
+                        .write(otw.as_ref())
+                        .map_err(|_| Error::SerialWrite)?;
+                    return Err(e);
+                } else {
+                    let otw =
+                        OTW::new(Cmd::Result, Data::Result(Response::Ok))?
+                            .to_vec()?;
+                    self.serial
+                        .write(otw.as_ref())
+                        .map_err(|_| Error::SerialWrite)?;
+                }
             }
-        }
-        Cmd::Stats | Cmd::Config | Cmd::Result => (),
-    };
+            Cmd::SetStandby => {
+                if let Data::U64(sleep_after) = otw_in.data() {
+                    config.sleep_after_ms = sleep_after;
+                    let otw =
+                        OTW::new(Cmd::Result, Data::Result(Response::Ok))?
+                            .to_vec()?;
+                    self.serial
+                        .write(otw.as_ref())
+                        .map_err(|_| Error::SerialWrite)?;
+                }
+            }
+            Cmd::Stats | Cmd::Config | Cmd::Result => (),
+        };
 
-    Ok(())
+        Ok(())
+    }
 }
